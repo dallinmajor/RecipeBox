f6fff340c28bceed822cf14b3162a4a8de08ca9a
Dally Wally <dallin.watac.major@gmail.com>
2018-11-25 15:52:45 -0700

    User Routes

--- routes/API/userRoutes.js
+++ routes/API/userRoutes.js
@@ -3,4 +3,15 @@ const UserController = require('../../controllers/userController');
 
 // ROUTE /api/user...
 
+router.route('/')
+    .post(UserController.create)
+
+router.route('/all')
+    .get(UserController.findAll)
+
+router.route('/:id')
+    .get(UserController.findById)
+    .put(UserController.update)
+    .delete(UserController.remove)
+
 module.exports = router;
