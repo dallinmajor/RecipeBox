24f451a3ef845d6d522454443ec7a9441a4d9a7a
Dally Wally <dallin.watac.major@gmail.com>
2018-11-25 06:38:52 -0700

    index.js for models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,9 @@
 const User = require('./user');
 const Category = require('./categories');
+const Recipe = require('./recipes');
+
+module.exports = {
+    User: User,
+    Category: Category,
+    Recipe: Recipe
+}
