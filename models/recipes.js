e430c5736c5944c1122e437c1214b6696e15e0a8
Dally Wally <dallin.watac.major@gmail.com>
2018-11-25 06:29:13 -0700

    finished up recipe Schema

--- models/recipes.js
+++ models/recipes.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema();
 
-const recipeSchema = new Schema ({
+const RecipeSchema = new Schema ({
     name: {
         type: String,
         require: true
+    },
+    recipe: {
+        type: String,
+        require: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-})
+});
+
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = Recipe;
