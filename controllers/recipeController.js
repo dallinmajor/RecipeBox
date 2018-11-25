68fde119fe95053e0fecbd68058f119b3e02d705
Dally Wally <dallin.watac.major@gmail.com>
2018-11-25 08:56:25 -0700

    find all for recipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,7 +2,11 @@ const db = require('../models');
 
 module.export = {
     findAll: () => {
-
+        db.User
+            .findById(req.params.userId, 'recipes')
+            .populate('Recipe')
+            .then(recipes => res.json(recipes))
+            .catch(err => res.status(402).json(err));
     },
     findById: () => {
 
